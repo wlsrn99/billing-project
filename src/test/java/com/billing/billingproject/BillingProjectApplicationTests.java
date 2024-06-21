6f5c4265606981a396435501120463eabcac259b
wlsrn99 <wlsrn331@gmail.com>
2024-06-21 15:44:33 +0900

    first commit

---
+++ src/test/java/com/billing/billingproject/BillingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.billingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
