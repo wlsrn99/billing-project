44de952549d261c4f5b9c5e31c99c7373e3be99f
wlsrn99 <wlsrn331@gmail.com>
2024-06-21 22:40:08 +0900

    feat: CI실행 테스트

--- src/test/java/com/billing/billingproject/basictest/UserRepositoryTest.java
+++ src/test/java/com/billing/billingproject/basictest/UserRepositoryTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.billing.billingproject.user.entity.User;
