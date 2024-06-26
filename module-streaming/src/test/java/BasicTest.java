2f43d124b96fa458a289f026e309d690c250c0f3
wlsrn99 <wlsrn331@gmail.com>
2024-06-26 13:21:38 +0900

    feature: CI테스트 테스트코드에 프로파일 명시하는 어노테이션 추가

--- module-streaming/src/test/java/BasicTest.java
+++ module-streaming/src/test/java/BasicTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.streaming.StremingProjectApplication;
 
 @SpringBootTest(classes = StremingProjectApplication.class)
+@ActiveProfiles("test")
 public class BasicTest {
 	@Test
 	void contextLoads() {
